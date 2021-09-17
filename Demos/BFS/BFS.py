7df8362f7138eb23e27bd16fb71f3faa9058574b
Muhammad Ahsan Naeem <mahsan.naeem@gmail.com>
2021-09-17 22:36:17 +0500

    Create BFS.py

---
+++ Demos/BFS/BFS.py
@@ -0,0 +1,42 @@
+from pyamze import maze,agent,COLOR,textLabel
+def BFS(m):
+    start=(m.rows,m.cols)
+    frontier=[start]
+    explored=[start]
+    bfsPath={}
+    while len(frontier)>0:
+        currCell=frontier.pop(0)
+        if currCell==(1,1):
+            break
+        for d in 'ESNW':
+            if m.maze_map[currCell][d]==True:
+                if d=='E':
+                    childCell=(currCell[0],currCell[1]+1)
+                elif d=='W':
+                    childCell=(currCell[0],currCell[1]-1)
+                elif d=='N':
+                    childCell=(currCell[0]-1,currCell[1])
+                elif d=='S':
+                    childCell=(currCell[0]+1,currCell[1])
+                if childCell in explored:
+                    continue
+                frontier.append(childCell)
+                explored.append(childCell)
+                bfsPath[childCell]=currCell
+    fwdPath={}
+    cell=(1,1)
+    while cell!=start:
+        fwdPath[bfsPath[cell]]=cell
+        cell=bfsPath[cell]
+    return fwdPath
+
+if __name__=='__main__':
+    m=maze(5,7)
+    m.CreateMaze(loopPercent=40)
+    path=BFS(m)
+
+    a=agent(m,footprints=True,filled=True)
+    m.tracePath({a:path})
+    l=textLabel(m,'Length of Shortest Path',len(path)+1)
+
+    m.run()
